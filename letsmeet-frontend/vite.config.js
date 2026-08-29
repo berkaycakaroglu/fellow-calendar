11a6d49609b3815c99b251e73280303b63a31455
berkaycakaroglu <berkaycakaroglu@gmail.com>
2026-08-29 20:20:38 +0300

    chore: cleanup git cache and ignore node_modules

--- letsmeet-frontend/vite.config.js
+++ letsmeet-frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   // GitHub Pages repo adınızı buraya yazın (Örn: repo adınız letsmeet-frontend ise)
-  base: '/lets-meet/',
+  base: '/letsmeet/',
   plugins: [react()],
   server: {
     port: 5173,
